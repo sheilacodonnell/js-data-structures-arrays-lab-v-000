97a3c72cf165c65c746c8ae6c7dd90d77be6e4fc
Sheila O'Donnell <sheila@contexttravel.com>
2018-09-11 22:39:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function destructivelyPrependDriver(name) {
 function destructivelyRemoveLastDriver(name) {
   drivers.pop(name)
 }
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.
+}
